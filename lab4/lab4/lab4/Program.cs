e51320c028034f88b3c1b4883a3b8555956de52b
Dzmitry Yermalovich <ermolovichdima22@gmail.com>
2021-03-27 00:53:58 +0300

    result lab4

--- lab4/lab4/lab4/Program.cs
+++ lab4/lab4/lab4/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using keyLog;
+
 namespace lab4
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            keyLogger.run();
+            keyLog.keyLogger.run();
         }
     }
 }
